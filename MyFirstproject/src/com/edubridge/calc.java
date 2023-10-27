efc54c978606a8b22a58fbd2d20202c3b6bb0b69
ASUS <ASUS@LAPTOP-DJAP4A66>
2023-10-27 16:35:09 +0530

    FIRST COMMIT

---
+++ MyFirstproject/src/com/edubridge/calc.java
@@ -0,0 +1,11 @@
+package com.edubridge;
+
+public class calc {
+	public static void findSquare(int num) {
+		System.out.println(num*num);
+		}
+	public static void findCube(int num) {
+		System.out.println(num*num *num);
+		}
+
+}
