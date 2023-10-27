729dbd22a14ad8fe23de6a63eb07b29af56b23e5
ASUS <ASUS@LAPTOP-DJAP4A66>
2023-10-27 17:00:25 +0530

    findSum

--- MyFirstproject/src/com/edubridge/calc.java
+++ MyFirstproject/src/com/edubridge/calc.java
@@ -4,6 +4,9 @@ public class calc {
 	public static void findSquare(int num) {
 		System.out.println(num*num);
 		}
+	public static void findSum(int num1,int num21) {
+		System.out.println(num1+num2);
+		}
 	//nothing
 	public static void findCube(int num) {
 		System.out.println(num*num *num);
